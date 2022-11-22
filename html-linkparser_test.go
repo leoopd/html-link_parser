0ad8d9dfbe761fc50cfd2f38f7fcacb54e2ca88d
leo <leo.opdenplatz@gmail.com>
2022-11-22 14:31:34 +0100

    added a test for the HtmlParser

--- html-linkparser_test.go
+++ html-linkparser_test.go
@@ -28,10 +28,23 @@ func TestHtmlReader(t *testing.T) {
 func TestHtmlParser(t *testing.T) {
 
 	readHtml := "<html>\n<head>\n<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\">\n</head>\n<body>\n<h1>Social stuffs</h1>\n<div>\n<a href=\"https://www.twitter.com/joncalhoun\">\nCheck me out on twitter\n<i class=\"fa fa-twitter\" aria-hidden=\"true\"></i>\n</a>\n<a href=\"https://github.com/gophercises\">\nGophercises is on <strong>Github</strong>!\n</a>\n</div>\n</body>\n</html>"
-	var want html.Node = html.Node{Parent:(*html.Node)(nil), FirstChild:(*html.Node)(nil), LastChild:(*html.Node)(nil), PrevSibling:(*html.Node)(nil), NextSibling:(*html.Node)(nil), Type:0x2, DataAtom:0x0, Data:"", Namespace:"", Attr:[]html.Attribute(nil)}
+	var want *html.Node = &html.Node{Parent: (*html.Node)(nil), FirstChild: (*html.Node)(nil), LastChild: (*html.Node)(nil), PrevSibling: (*html.Node)(nil), NextSibling: (*html.Node)(nil), Type: 0x2, DataAtom: 0x0, Data: "", Namespace: "", Attr: []html.Attribute(nil)}
 
 	result, err := u.HtmlParser(readHtml)
-	if result != &want || err != nil {
+	if result.Type != want.Type || err != nil {
 		t.Fatalf("HtmlReader(s) = %v, %v, want match for %#v, nil", result, err, want)
 	}
+	if result.DataAtom != want.DataAtom || err != nil {
+		t.Fatalf("HtmlReader(s) = %v, %v, want match for %#v, nil", result, err, want)
+	}
+	if result.Data != want.Data || err != nil {
+		t.Fatalf("HtmlReader(s) = %v, %v, want match for %#v, nil", result, err, want)
+	}
+	if result.Namespace != want.Namespace || err != nil {
+		t.Fatalf("HtmlReader(s) = %v, %v, want match for %#v, nil", result, err, want)
+	}
+}
+
+func TestTagParser (t *testing.T) {
+	
 }
